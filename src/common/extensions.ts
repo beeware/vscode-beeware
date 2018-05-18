e185792382122f1d3afca753b08d98fe8d37eb89
Don Jayamanne <don.jayamanne@yahoo.com>
2018-05-18 14:28:29 -0700

    Fix string split

--- src/common/extensions.ts
+++ src/common/extensions.ts
@@ -33,10 +33,10 @@ declare interface String {
  * By default lines are trimmed and empty lines are removed.
  * @param {SplitLinesOptions=} splitOptions - Options used for splitting the string.
  */
-String.prototype.splitLines = function (this: string, splitOptions: { trim: boolean, removeEmptyEntries: boolean } = { removeEmptyEntries: true, trim: true }): string[] {
+String.prototype.splitLines = function (this: string, splitOptions: { trim: boolean; removeEmptyEntries: boolean } = { removeEmptyEntries: true, trim: true }): string[] {
     let lines = this.split(/\r?\n/g);
     if (splitOptions && splitOptions.trim) {
-        lines = lines.filter(line => line.trim());
+        lines = lines.map(line => line.trim());
     }
     if (splitOptions && splitOptions.removeEmptyEntries) {
         lines = lines.filter(line => line.length > 0);
