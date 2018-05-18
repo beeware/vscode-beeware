76045a315418da6f0cc1d8bf30603a82fe6560f5
Don Jayamanne <don.jayamanne@yahoo.com>
2018-05-18 14:28:45 -0700

    Add ability to update settings

--- src/common/configuration/service.ts
+++ src/common/configuration/service.ts
@@ -2,11 +2,11 @@
 // Licensed under the MIT License.
 
 import { inject, injectable } from 'inversify';
-import { Uri } from 'vscode';
+import { ConfigurationTarget, Uri, workspace } from 'vscode';
 import { IServiceContainer } from '../../ioc/types';
 import { IWorkspaceService } from '../application/types';
 import { BeeWareSettings } from './settings';
-import { IBeeWareSettings, IConfigurationService } from './types';
+import { IBeeWareSettings, IConfigurationService, SettingsProperty } from './types';
 
 @injectable()
 export class ConfigurationService implements IConfigurationService {
@@ -18,4 +18,18 @@ export class ConfigurationService implements IConfigurationService {
         const workspaceFolder = resource ? this.workspaceService.getWorkspaceFolder(resource) : undefined;
         return new BeeWareSettings(this.serviceContainer, workspaceFolder ? workspaceFolder.uri : undefined);
     }
+    public async updateSettingAsync(setting: SettingsProperty, value?: {}, resource?: Uri, configTarget?: ConfigurationTarget): Promise<void> {
+        const beewareConfig = workspace.getConfiguration('beeware', resource);
+        const currentValue = beewareConfig.inspect(setting);
+
+        if (currentValue !== undefined &&
+            ((configTarget === ConfigurationTarget.Global && currentValue.globalValue === value) ||
+                (configTarget === ConfigurationTarget.Workspace && currentValue.workspaceValue === value) ||
+                (configTarget === ConfigurationTarget.WorkspaceFolder && currentValue.workspaceFolderValue === value))) {
+            return;
+        }
+
+        await beewareConfig.update(setting, value, configTarget);
+    }
+
 }
--- src/common/configuration/types.ts
+++ src/common/configuration/types.ts
@@ -1,15 +1,19 @@
-import { Uri } from 'vscode';
+import { ConfigurationTarget, Uri } from 'vscode';
 
 export const IConfigurationService = Symbol('IConfigurationService');
+export type SettingsProperty = keyof IBeeWareSettings;
 
 export interface IConfigurationService {
     getSettings(resource?: Uri): IBeeWareSettings;
+    updateSettingAsync(setting: SettingsProperty, value?: {}, resource?: Uri, configTarget?: ConfigurationTarget): Promise<void>;
 }
 
 export const IBeeWareSettings = Symbol('IBeeWareSettings');
 
 export interface IBeeWareSettings {
     beewarePath: string;
+    name?: string;
+    formalName?: string;
     pythonPath: string;
     cookiecutterTemplateRepoUrl: string;
 }
